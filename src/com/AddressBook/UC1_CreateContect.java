a7217da93dba7d31526395ce869cb1a46c082dd0
Vishva <gvishva2601@gmail.com>
2022-11-05 16:51:30 +0530

    Create Contact in Address Book

---
+++ src/com/AddressBook/UC1_CreateContect.java
@@ -0,0 +1,13 @@
+package com.AddressBook;
+
+public class UC1_CreateContect
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Address Book");
+        Contacts obj = new Contacts("vishva", "Santoki", "Dahisar", "Mumbai", "Maharashtra", "400068", "7359294419", "ghetiyavishva2601@gamil.com");
+        System.out.println(obj);
+
+    }
+}
+
